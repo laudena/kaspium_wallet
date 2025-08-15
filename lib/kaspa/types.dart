b642e4df5f08b2e4548d71842f8d349e53fb927d
laudena <laudena@gmail.com>
2025-08-16 00:36:28 +1000

    Add parser for kaspaterminal URIs

--- lib/kaspa/types.dart
+++ lib/kaspa/types.dart
@@ -3,4 +3,5 @@ export 'types/address_balance.dart';
 export 'types/address_prefix.dart';
 export 'types/amount.dart';
 export 'types/kaspa_uri.dart';
+export 'types/kaspa_terminal_uri.dart';
 export 'types/token_info.dart';
